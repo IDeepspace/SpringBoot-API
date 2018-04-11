bf5209fde34d4ca469c9d63c480d67b9714457e5
IDeepspace <cxin1427@qq.com>
2018-04-11 21:48:30 +0800

    添加新增一名employee的api

--- src/main/java/com/springbootapi/demo/EmployeeControlllor.java
+++ src/main/java/com/springbootapi/demo/EmployeeControlllor.java
@@ -2,7 +2,11 @@ package com.springbootapi.demo;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.List;
 
 @RestController
@@ -11,8 +15,31 @@ public class EmployeeControlllor {
   @Autowired
   private EmployeeRepository employeeRepository;
 
+  //查询所有employee列表
   @GetMapping(value = "/employees")
   public List<Employee> employeeList() {
     return employeeRepository.findAll();
   }
+
+  //新增一名employee
+  @PostMapping(value = "/employees")
+  public Employee employeeAdd(@RequestParam("name") String name,
+                            @RequestParam("age") Integer age,
+                            @RequestParam("gender") String gender) {
+
+    Employee employee = new Employee();
+
+    employee.setName(name);
+    employee.setAge(age);
+    employee.setGender(gender);
+
+    return employeeRepository.save(employee);
+
+  }
+
+//  //查询一名employee
+//  @GetMapping(value = "/employees/${id}")
+//  public Employee employeeFindOne(@PathVariable("id") Integer id) {
+//    return employeeRepository.getOne(id);
+//  }
 }
--- src/main/java/com/springbootapi/demo/EmployeeRepository.java
+++ src/main/java/com/springbootapi/demo/EmployeeRepository.java
@@ -2,6 +2,6 @@ package com.springbootapi.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<com.springbootapi.demo.Employee, Integer> {
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
